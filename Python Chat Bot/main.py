c0580ea1c4cb0519e193c3047a3d6569bd71e49c
Fredrich Bernard Onduru <113997263+Bernard8ball@users.noreply.github.com>
2023-01-17 16:18:24 +0300

    Update main.py

--- Python Chat Bot/main.py
+++ Python Chat Bot/main.py
@@ -94,8 +94,16 @@ def chat():
         inp = input("You: ")
         if inp.lower() == "quit":
             break
-        results = model.predict([bag_of_words(inp, words)])
-        print(results)
+        results = model.predict([bag_of_words(inp , words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        for tg in data ["intents"]:
+            if tg ['tag'] == tag:
+                responses = tg['responses']
+                print(random.choice(responses))
+
+chat()
 
 
 
